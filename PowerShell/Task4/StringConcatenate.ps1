18f2008b1e20f4121130696a0e6aa2f4615179c5
naemi-r <naemi.ross@noseryoung.com>
2024-03-01 11:06:19 +0100

    verbesserte Grundfunktion mit Winner Funktion und counter

--- PowerShell/Task4/StringConcatenate.ps1
+++ PowerShell/Task4/StringConcatenate.ps1
@@ -1,112 +1,50 @@
 Write-Host "Welcom to the string concatenate game!"
 Write-Host "Rules"
 Write-Host "You get a word, now write in a word that starts with the letter the word ended with before."
-$starterString = "Hello"
-Write-Host "$starterString"
+$oldInput = "Hello"
+Write-Host $oldInput
+
+$dateipfad = "C:\Users\naemi\BLJ2024NaRos\PowerShell\Task4\wordlist.txt"
+$inhaltDerDatei = Get-Content -Path $dateipfad #liest den Inhalt der Datei
 
 
-    $dateipfad = "C:\Users\naemi\BLJ2024NaRos\PowerShell\Task4\wordlist.txt"
-    $inhaltDerDatei = Get-Content -Path $dateipfad #liest den Inhalt der Datei
-    $userInput = Read-Host "Your turn"
-    $userFirstLetter = $userInput[0]
-    $userLastLetter = $userInput[-1]
-    $starterLastLetter = $starterString[-1]
-    if ($starterLastLetter -eq $userFirstLetter)
-    {
-        $inputIsValid = $false
-        foreach ($wort in $inhaltDerDatei) { #damit jedes Wort in Datei verglichen wird mit userInpurt
-            if ($userInput -eq $wort) {
-                $inputIsValid = $true
-                break
-            }
-        }
-        if ($inputIsValid) {
-            Write-Host "Good :)"
-        } else {
-            Write-Host "Invalid input :("
-        }
-    }
-    else
-    {
-        Write-Host "Not good :("
-    }
-
-
-
-    $userInput1 = Read-Host "Your turn"
-    $user1FirstLetter = $userInput1[0]
-    $userLastLetter = $userInput[-1]
-
-    if ($user1FirstLetter -eq $userLastLetter)
-    {
-        $inputIsValid = $false
-
-        foreach ($wort in $inhaltDerDatei)
-        {
-            if ($userInputAgain -eq $wort)
-            {
-                $inputIsValid = $true
-                break
-            }
-        }
-#es ist so das beim zweiten durchgang if nicht mehr erfüllt wird und dann auf else geht und deswegen not good schreibt
-
-        if ($inputIsValid)
-        {
-            Write-Host "Good :)"
-        } else
-        {
-            Write-Host "Invalid input :("
-        }
-
-    }
-    else
-    {
-        Write-Host "Not good :("
-    }
-    $inputIsValid = $false
-
-$bedingung = $false
+$counter = 0
 
 do
 {
+$newInput = Read-Host "Your turn"
+
+if ($newInput[0] -eq $oldInput[-1])
+{
     $inputIsValid = $false
-    $inputIsValid = $null
-    $userInput2 = $null
-    $user2FirstLetter = $null
-    $user1LastLetter = $null
-    $bedingung = $false
-    $userInput2 = Read-Host "Your turn"
-    $user2FirstLetter = $userInput2[0]
-    $user1LastLetter = $userInput1[-1]
 
-    if ($user2FirstLetter -eq $user1LastLetter)
+    foreach ($wort in $inhaltDerDatei)
     {
-        $inputIsValid = $false
-
-        foreach ($wort in $inhaltDerDatei)
+        if ($newInput -eq $wort)
         {
-            if ($userInputAgain -eq $wort)
-            {
-                $inputIsValid = $true
-                break
-            }
+            $inputIsValid = $true
+            break
         }
-#es ist so das beim zweiten durchgang if nicht mehr erfüllt wird und dann auf else geht und deswegen not good schreibt
-
-        if ($inputIsValid)
-        {
-            Write-Host "Good :)"
-        } else
-        {
-            Write-Host "Invalid input :("
-        }
-
     }
-    else
+    if ($inputIsValid)
     {
-        Write-Host "Not good :("
+        Write-Host "Good :)"
+        $oldInput = $newInput
+        $counter = $counter +1
+    } else
+    {
+        Write-Host "The word is not on the list"
+        $counter = 0
     }
-    $userInput1 = $userInput2
-$bedingung = $false
-} while (-not $bedingung)
+
+}
+else
+{
+    Write-Host "You have entered a word that is not comfortable (read the rules)"
+    $counter = 0
+}
+
+
+} while ($counter -lt 20)
+
+Write-Host "Congratulations, you are the winner!"
