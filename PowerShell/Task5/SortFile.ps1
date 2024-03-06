7eebec1ef2f3fdd81bf21b5e4fbe4e764c140b65
naemi-r <naemi.ross@noseryoung.com>
2024-03-06 09:05:53 +0100

    Anfang von Task 6 SortFile

---
+++ PowerShell/Task5/SortFile.ps1
@@ -0,0 +1,12 @@
+Get-ChildItem C:\Users\naemi
+Get-ChildItem -Path ".\TestingPurpose" -Recurse -File |  
+    ForEach-Object {
+        $zahl = ($_.Name -replace '\D','').Trim()
+if ($i = $zahl % 2) {
+    Move-Item -path C:\Users\naemi\BLJ2024NaRos\PowerShell\Task5\TestingPurpose\SubFolder2\$i.txt -Destination .\TestingPurpose\SubFolder1
+}else {
+    Move-Item  -path C:\Users\naemi\BLJ2024NaRos\PowerShell\Task5\TestingPurpose\SubFolder1\$i.txt -Destination .\TestingPurpose\SubFolder2
+}    
+consol.log
+    }
+ 
